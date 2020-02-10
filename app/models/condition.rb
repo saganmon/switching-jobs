54f5f26e4925bc157ea7559ea0a5cf83b6d2482f
saganmon <fevers24@gmail.com>
2020-02-10 18:51:00 +0900

    validation

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,7 +2,7 @@ class Condition < ApplicationRecord
   belongs_to :office
   belongs_to :user
 
-  validates :phase, :date, :result, presence: true
+  validates :phase, :date, :result, :office_id, :user_id, presence: true
   validates :memo, length: { maximum: 100, too_long: "最大%{count}文字まで使えます" }
 
   enum phase: [:pre_stage, :first_stage, :second_stage, :final_stage]
