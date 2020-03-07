3a61ac23e1f8983cb8b59e7b704150df4b863ac4
saganmon <fevers24@gmail.com>
2020-03-07 20:40:42 +0900

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '54.92.116.119', user: 'ec2-user', roles: %w{app db web}
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
+# set :rails_env, "production"
+# set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
