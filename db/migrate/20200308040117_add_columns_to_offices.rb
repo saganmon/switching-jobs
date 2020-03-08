dc7fcf39a2d3bdf1b18725f282cc2226f93a0e35
saganmon <fevers24@gmail.com>
2020-03-08 13:27:19 +0900

    test_helper

--- db/migrate/20200308040117_add_columns_to_offices.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToOffices < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :offices, :people, :integer
-    remove_column :offices, :foundation, :integer
-    add_column :offices, :poople,     :integer
-    add_column :offices, :foundation, :integer
-  end
-end
