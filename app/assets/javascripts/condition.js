c2f482873abdbd9a1d16660d635a7a1eb6a3da33
saganmon <fevers24@gmail.com>
2020-03-08 19:02:11 +0900

    revised

--- app/assets/javascripts/condition.js
+++ app/assets/javascripts/condition.js
@@ -1,46 +1,39 @@
 $(function(){
-  function buildHTML(condition){
+  function buildHTML(condition) {
       `<tbody class="conditions-table__body">
-        <tr class="conditions-table__column">
+        <tr class="conditions-table__column" style="background-color: ghostwhite;">
           <td class="conditions-table__column--status">
             ${condition.phase}
           </td>
-          <td class:="conditions-table__column--result">
+          <td class="conditions-table__column--result">
             ${condition.result}
           </td>
-          <td class:="conditions-table__column--company-name">
+          <td class="conditions-table__column--company-name">
             ${condition.company_name}
           </td>
-          <td class:="conditions-table__column--company-area">
+          <td class="conditions-table__column--company-area">
             ${condition.area}
           </td>
-          <td class:="conditions-table__column--date">
+          <td class="conditions-table__column--date">
             ${condition.date}
           </td>
-          <td class:="conditions-table__column--memo">
-            ${condition.memo}
+          <td class="conditions-table__column--memo">
+          ${condition.memo}
           </td>
-          <td class:="conditions-table__column--edit">
-            <a href="/conditions/${condition.id}/edit">
-              <i class="fas fa-edit button">
-              </i>
-            </a>
-            <a rel="nofollow" data-method="delete" href="/conditions/${condition.id}">
-              <i class="fas fa-trash-alt button">
-              </i>
-            </a>
+          <td class="conditions-table__column--edit">
+            <a href="/conditions/50/edit"><i class="fas fa-edit button"></i></a>
+            <a rel="nofollow" data-method="delete" href="/conditions/50"><i class="fas fa-trash-alt button"></i></a>
           </td>
         </tr>
       </tbody>`
-    return html
-  }
-  
+    return html;
+  };
+
   $('.body-conditions__new--button').on('click', function(){
     $('.modal-overlay').fadeIn("slow");
     $('#new_condition').on('submit', function(e) {
       e.preventDefault();
       var formData = new FormData(this);
-      // console.log(formData);
       var url = $(this).attr('action');
       $.ajax({
         url: url,
@@ -50,16 +43,16 @@ $(function(){
         processData: false,
         contentType: false
       })
-      .done(function(data){
-        var html = buildHTML(data);
+      .done(function(condition){
+        var html = buildHTML(condition);
         $('.conditions-table').append(html);
       })
       .fail(function(){
         alert('Ajax通信に失敗しました');
       })
       $('.modal-overlay').fadeOut("slow");
-    })
-  })
+    });
+  });
 
   $('.body-conditions__find--box').on('keyup', function() {
     var keyword = $(this).val();
@@ -71,16 +64,18 @@ $(function(){
         if($(this).text().match(regExp)) {
           tr.show();
         }
-      })
-    })
-  })
+      });
+    });
+  });
 
   $('.conditions-table__column').mouseover(function(){
     // console.log("mouseover");
     $(this).css('backgroundColor', 'aquamarine')
-  })
+  });
 
   $('.conditions-table__column').mouseleave(function(){
     $(this).css('backgroundColor', 'ghostwhite')
-  })
-})
+  });
+});
+
+
